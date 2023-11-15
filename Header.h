06e984ba1e53bcbbc8cc55734e250d97712d45bc
VanHoangDH52005828 <gloomybeezz@gmail.com>
2023-11-15 10:09:11 +0700

    hoan thanh cau

--- Header.h
+++ Header.h
@@ -4,4 +4,12 @@ using namespace std;
 const int MAX = 100;
 
 void nhap(int a[], int n);
+int tinhTong(int a[]){
+    int tong=0;
+    for (int i = 0; i < length(a); i++)
+	{
+		tong+=a[i];
+	}
+    return tong;
+}
 //Viet ham tinh tong cac phan tu co trong mang
--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	int a[MAX];
 	nhap(a, 10);
+	int ketQua= tinhTong(a);
 	//Goi ham tinh tong cac phan tu co trong mang
 	return 0;
 }
